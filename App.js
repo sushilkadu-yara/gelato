a86647328db6b3192efde4f952e5814cfe5e423e
Sushil Kadu <70520130+sushilkadu-yara@users.noreply.github.com>
2021-03-02 08:51:53 +0530

    Reformatted the code

--- App.js
+++ App.js
@@ -1,29 +1,29 @@
-import React from "react";
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
-import { Provider } from "./src/context/PhotoContext";
-import PhotoDetailsScreen from "./src/screens/PhotoDetailsScreen";
-import PhotoListScreen from "./src/screens/PhotoListScreen";
+import React from 'react'
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import { Provider } from './src/context/PhotoContext'
+import PhotoDetailsScreen from './src/screens/PhotoDetailsScreen'
+import PhotoListScreen from './src/screens/PhotoListScreen'
 
 const navigator = createStackNavigator(
   {
     PhotoList: PhotoListScreen,
-    PhotoDetails: PhotoDetailsScreen,
+    PhotoDetails: PhotoDetailsScreen
   },
   {
-    initialRouteName: "PhotoList",
+    initialRouteName: 'PhotoList',
     defaultNavigationOptions: {
-      title: "Lorem Picsum",
-    },
+      title: 'Lorem Picsum'
+    }
   }
-);
+)
 
-const App = createAppContainer(navigator);
+const App = createAppContainer(navigator)
 
 export default () => {
   return (
     <Provider>
       <App />
     </Provider>
-  );
-};
+  )
+}
